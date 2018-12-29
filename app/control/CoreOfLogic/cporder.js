d8cc6cca81e257692d0b5426f077f79122be9822
pumi365 <pumi365>
2018-12-29 10:03:48 +0800

    ...

--- app/control/CoreOfLogic/cporder.js
+++ app/control/CoreOfLogic/cporder.js
@@ -2,23 +2,15 @@ let facade = require('gamecloud');
 let tableType = require('../../util/tabletype');
 let tableField = require('../../util/tablefield');
 let randomHelp = require('../../util/randomHelp');
+let remoteSetup = require('../../util/gamegold_cp');
 //引入工具包
 var toolkit = require('gamegoldtoolkit')
 //创建授权式连接器实例
 var remote = new toolkit.conn();
 //兼容性设置，提供模拟浏览器环境中的 fetch 函数
 remote.setFetch(require('node-fetch')) 
-/*
-remote.setup({
-        type:   'testnet',
-        ip:     '114.116.12.248',     //远程服务器地址
-        head:   'http',               //远程服务器通讯协议，分为 http 和 https
-        id:     'primary',            //默认访问的钱包编号
-        apiKey: 'bookmansoft',        //远程服务器基本校验密码
-        cid:    'xxxxxxxx-game-gold-root-xxxxxxxxxxxx', //授权节点编号，用于访问远程钱包时的认证
-        token:  '03aee0ed00c6ad4819641c7201f4f44289564ac4e816918828703eecf49e382d08', //授权节点令牌固定量，用于访问远程钱包时的认证
-});
-*/
+remote.setup(remoteSetup);
+
 /**
  * 游戏用户
  * Create by gamegold Fuzhou on 2018-11-27
