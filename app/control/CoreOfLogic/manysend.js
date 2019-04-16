07caf01097ab359a6f9e9952978c0895becef0f7
ThomasFuzhou <29013551@qq.com>
2019-04-16 15:15:15 +0800

    fix

--- app/control/CoreOfLogic/manysend.js
+++ app/control/CoreOfLogic/manysend.js
@@ -245,6 +245,7 @@ class manysend extends facade.Control {
             //发送到指定账号
             //cid 用固定的manyagent创建 687a8b10-5a91-11e9-9a3f-bfc33c24ad96
             let retCp=await gamegoldHelp.execute('cp.byName', ['manyagent']);
+            console.log("247",retCp);
             console.log("248 cpid:",retCp.cid);
             let agent_cid=retCp.cid;
             let agent_uid=manysend.ormAttr("id");//对应此地址的id
