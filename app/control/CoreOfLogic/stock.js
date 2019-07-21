9828860d164e024486bbdb8faabbb3107c031d73
bookman <ceo@920.cc>
2019-07-21 19:19:24 +0800

    为 CoreOfLogic 添加配置表管理机制，客户端通过 config.get 获取配置表并缓存

--- app/control/CoreOfLogic/stock.js
+++ app/control/CoreOfLogic/stock.js
@@ -6,6 +6,46 @@ let {TableType, TableField} = facade.const;
  */
 class stock extends facade.Control
 {
+    /**
+     * 从数据库中获取列表
+     * 客户端直接调用此方法
+     * @param {*} user 
+     * @param {*} objData 查询及翻页参数，等整体调通以后再细化。
+     */
+    ListRecord(user, objData) {
+        objData = objData || {};
+        let currentPage = objData.currentPage;
+        if (Number.isNaN(parseInt(currentPage))) {
+            currentPage = 1;
+        }
+        //构造查询条件
+        let paramArray = [];
+        if (!!objData.cp_text) {
+            paramArray.push(['cp_text', objData.cp_text]);
+        }
+        if (!!objData.audit_state_id) {
+            paramArray.push(['audit_state_id', objData.audit_state_id]);
+        }
+
+        let muster = this.core.GetMapping(TableType.StockBase)
+            .groupOf() // 将 Mapping 对象转化为 Collection 对象，如果 Mapping 对象支持分组，可以带分组参数调用
+            .where(paramArray)
+            .orderby('id', 'desc') //根据id字段倒叙排列
+            .paginate(10, currentPage);
+
+        let list = [];
+        let $idx = (muster.pageCur - 1) * muster.pageSize;
+        for (let $value of muster.records(TableField.StockBase)) {
+            $value['rank'] = $idx++;
+            list.push($value);
+        }
+
+        return {code: 0, data: {
+            total: muster.pageNum,
+            page: muster.pageCur,
+            list: list}};
+    }
+
     //众筹详情
     async StockSale(user, params) {
         let uid = user.id
