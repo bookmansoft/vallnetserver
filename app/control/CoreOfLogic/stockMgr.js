0cb45648a360a437a873efa71cd88b5054e4abc0
bookman <ceo@920.cc>
2019-08-25 20:48:23 +0800

    新增凭证无偿转让功能 修复 UserStockLogs 函数上的一处笔误

--- app/control/CoreOfLogic/stockMgr.js
+++ app/control/CoreOfLogic/stockMgr.js
@@ -6,23 +6,22 @@ let remoteSetup = facade.ini.servers["Index"][1].node; //全节点配置信息
  * 游戏的控制器
  * Updated on 2018-11-19.
  */
-class cpstockbase extends facade.Control {
+class stockMgr extends facade.Control {
     /**
      * 向目标地址赠送凭证
      * @param {*} user      当前操作员，注意如果是系统管理员，要将账户切换为'game'
      * @param {*} objData 
      */
     async sendStock(user, objData) {
-        let account = user.cid;
-        if(account == remoteSetup.cid) {
-            account = 'game';
-        }
-
-        let ret = await this.core.service.RemoteNode.conn(user.cid).execute('stock.send', [
-            objData.params.cid, objData.params.num, objData.params.address, null, objData.params.srcAddr,
+        let ret = await this.core.service.gamegoldHelper.execute('stock.send', [
+            objData.params.cid, 
+            objData.params.num, 
+            objData.params.address, 
+            user.domainId, 
+            objData.params.srcAddr,
         ]);
 
-        return {code: ret.code}
+        return {code: ret.code};
     }
 
     /**
@@ -195,8 +194,8 @@ class cpstockbase extends facade.Control {
         let ret = await this.core.service.gamegoldHelper.execute('stock.record.wallet', [0, params.cid, 0, [['addr', params.addr]]]);
         if(ret.code == 0) {
             //弥补下协议上的差异
-            $data.total = ret.result.page;
-            $data.page = ret.result.cur;
+            ret.result.total = ret.result.page;
+            ret.result.page = ret.result.cur;
             //添加当前主网高度，作为时间基点使用
             ret.result.height = this.core.chain.height; 
 
@@ -212,4 +211,4 @@ class cpstockbase extends facade.Control {
     }    
 }
 
-exports = module.exports = cpstockbase;
+exports = module.exports = stockMgr;
